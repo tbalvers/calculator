0ce97fa7074e42b8c641b15576b602f24cbc76b1
Tim Balvers <128842895+tbalvers@users.noreply.github.com>
2023-04-30 11:54:36 -0400

    Run prettier on code

--- calculator.js
+++ calculator.js
@@ -7,70 +7,64 @@ let AC = document.querySelector('#AC');
 AC.addEventListener('click', clearDisplay);
 
 let numbers = document.querySelectorAll('.number');
-    numbers.forEach(button => {
-        button.addEventListener("click", calculateDisplay);
-    });
+numbers.forEach((button) => {
+    button.addEventListener('click', calculateDisplay);
+});
 
 let operators = document.querySelectorAll('.operator');
-    operators.forEach(operatorValue => {
-        operatorValue.addEventListener('click', getOperator); 
-    })
+operators.forEach((operatorValue) => {
+    operatorValue.addEventListener('click', getOperator);
+});
 
 let modulators = document.querySelectorAll('.modulator');
-    modulators.forEach(modulatorValue => {
-        modulatorValue.addEventListener('click', enactModulator); 
-    })
+modulators.forEach((modulatorValue) => {
+    modulatorValue.addEventListener('click', enactModulator);
+});
 
 let equals = document.querySelector('#equals');
-    equals.addEventListener('click', equalsCalculation);
+equals.addEventListener('click', equalsCalculation);
 
 let allButtons = document.querySelectorAll('.column');
-    allButtons.forEach(button => {
-        button.addEventListener("click", addHighlight);
-        button.addEventListener("transitionend", removeHighlight);
-    });
+allButtons.forEach((button) => {
+    button.addEventListener('click', addHighlight);
+    button.addEventListener('transitionend', removeHighlight);
+});
 
 function equalsCalculation(e) {
-    if (operator)
-    {
-    if (e && lastKeyPressed != 'equals' && numbersInCalculation.length >= 1)
-    {
-        if (displayValue != '')
-        {
-            numbersInCalculation.push(displayValue);
+    if (operator) {
+        if (
+            e &&
+            lastKeyPressed != 'equals' &&
+            numbersInCalculation.length >= 1
+        ) {
+            if (displayValue != '') {
+                numbersInCalculation.push(displayValue);
+            } else {
+                displayValue = '0';
+                populateDisplay();
+                lastKeyPressed = '';
+                numbersInCalculation = [];
+                operator = '';
+                console.log('reset');
+                return;
+            }
+        } else if (e) {
+            if ((lastKeyPressed = 'operator')) {
+                return;
+            }
         }
-        else
-        {
-            displayValue = '0';
+
+        displayValue = operate(operator, ...numbersInCalculation);
+        numbersInCalculation.splice(0, numbersInCalculation.length - 1);
+        numbersInCalculation.unshift(displayValue);
+        console.log(numbersInCalculation);
+        if (e) {
+            if (numbersInCalculation.length <= 1) {
+                return;
+            }
             populateDisplay();
-            lastKeyPressed = '';
-            numbersInCalculation = [];
-            operator = '';
-            console.log('reset');
-            return;
+            lastKeyPressed = 'equals';
         }
-    }
-    else if (e)
-    {
-        if (lastKeyPressed = 'operator')
-        {
-            return
-        }
-    }
-    
-    displayValue = operate(operator, ...numbersInCalculation);
-    numbersInCalculation.splice(0, numbersInCalculation.length - 1);
-    numbersInCalculation.unshift(displayValue);
-    console.log(numbersInCalculation);
-    if (e)
-    {
-        if (numbersInCalculation.length <= 1)
-        {
-            return;
-        }
-        populateDisplay();
-        lastKeyPressed = 'equals';
-    }
     }
 }
 
@@ -82,20 +76,18 @@ function clearDisplay() {
 }
 
 function calculateDisplay(event) {
-    if (event.target.textContent.includes(".") && displayValue.includes("."))
-    {
+    if (event.target.textContent.includes('.') && displayValue.includes('.')) {
         return;
     }
-    if (displayValue === '0' && event.target.textContent.includes('0'))
-    {
+    if (displayValue === '0' && event.target.textContent.includes('0')) {
         return;
-    }
-    else if (event.target.querySelector('.text') && displayValue.length < 7)
-    {
-        if (displayValue === '0' || lastKeyPressed === 'operator' || lastKeyPressed === 'equals')
-        {           
-            if (lastKeyPressed === 'equals')
-            {
+    } else if (event.target.querySelector('.text') && displayValue.length < 7) {
+        if (
+            displayValue === '0' ||
+            lastKeyPressed === 'operator' ||
+            lastKeyPressed === 'equals'
+        ) {
+            if (lastKeyPressed === 'equals') {
                 displayValue = '0';
                 populateDisplay();
                 lastKeyPressed = '';
@@ -104,18 +96,16 @@ function calculateDisplay(event) {
                 console.log('reset');
             }
             displayValue = event.target.querySelector('.text').textContent;
+        } else {
+            displayValue += event.target.querySelector('.text').textContent;
         }
-        else
-        {
-            displayValue += event.target.querySelector('.text').textContent; 
-        } 
-    }
-    else if (displayValue.length < 7)
-    {
-        if (displayValue === '0' || lastKeyPressed === 'operator' || lastKeyPressed === 'equals')
-        {
-            if (lastKeyPressed === 'equals')
-            {
+    } else if (displayValue.length < 7) {
+        if (
+            displayValue === '0' ||
+            lastKeyPressed === 'operator' ||
+            lastKeyPressed === 'equals'
+        ) {
+            if (lastKeyPressed === 'equals') {
                 displayValue = '0';
                 populateDisplay();
                 lastKeyPressed = '';
@@ -124,9 +114,7 @@ function calculateDisplay(event) {
                 console.log('reset');
             }
             displayValue = event.target.textContent;
-        }
-        else
-        {
+        } else {
             displayValue += event.target.textContent;
         }
     }
@@ -135,206 +123,158 @@ function calculateDisplay(event) {
 }
 
 function populateDisplay() {
-    const display = document.querySelector(".display .text");
+    const display = document.querySelector('.display .text');
     display.textContent = `${displayValue}`;
 }
 
 function add() {
-    total = Number(arguments[0])
-    for (let i = 1; i < arguments.length; i++)
-    {
-        total += Number(arguments[i])
+    total = Number(arguments[0]);
+    for (let i = 1; i < arguments.length; i++) {
+        total += Number(arguments[i]);
     }
-    if (typeof(total) === 'string')
-    {
+    if (typeof total === 'string') {
         return total;
-    }
-    else if (typeof(total) === 'number')
-    {
+    } else if (typeof total === 'number') {
         return total.toString();
-    }
-    else
-    {
+    } else {
         return displayValue;
     }
 }
 
 function subtract() {
-    total = arguments[0]
-    for (let i = 1; i < arguments.length; i++)
-    {
-        total -= arguments[i]
+    total = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        total -= arguments[i];
     }
-    if (typeof(total) === 'string')
-    {
+    if (typeof total === 'string') {
         return total;
-    }
-    else if (typeof(total) === 'number')
-    {
+    } else if (typeof total === 'number') {
         return total.toString();
-    }
-    else
-    {
+    } else {
         return displayValue;
     }
 }
 
 function divide() {
-    total = arguments[0]
-    for (let i = 1; i < arguments.length; i++)
-    {
-        total /= arguments[i]
+    total = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        total /= arguments[i];
     }
-    if (typeof(total) === 'string')
-    {
+    if (typeof total === 'string') {
         return total;
-    }
-    else if (typeof(total) === 'number')
-    {
+    } else if (typeof total === 'number') {
         return total.toString();
-    }
-    else
-    {
+    } else {
         return displayValue;
     }
 }
 
 function multiply() {
-    total = arguments[0]
-    for (let i = 1; i < arguments.length; i++)
-    {
-        total *= arguments[i]
+    total = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        total *= arguments[i];
     }
-    if (typeof(total) === 'string')
-    {
+    if (typeof total === 'string') {
         return total;
-    }
-    else if (typeof(total) === 'number')
-    {
+    } else if (typeof total === 'number') {
         return total.toString();
-    }
-    else
-    {
+    } else {
         return displayValue;
     }
 }
 
-function operate(operator, ...numbers)
-{
-    if (operator === "add")
-    {
+function operate(operator, ...numbers) {
+    if (operator === 'add') {
         return add(...numbers);
-    }
-    else if (operator === "subtract")
-    {
+    } else if (operator === 'subtract') {
         return subtract(...numbers);
-    }
-    else if (operator === "multiply")
-    {
+    } else if (operator === 'multiply') {
         return multiply(...numbers);
-    }
-    else if (operator === "divide")
-    {
+    } else if (operator === 'divide') {
         return divide(...numbers);
     }
 }
 
 function getOperator(e) {
-    if (displayValue != '')
-    {
+    if (displayValue != '') {
         numbersInCalculation.push(displayValue);
     }
     console.log(numbersInCalculation);
-    if (numbersInCalculation.length >= 2)
-    {
-        if (lastKeyPressed != 'equals')
-        {
+    if (numbersInCalculation.length >= 2) {
+        if (lastKeyPressed != 'equals') {
             equalsCalculation();
         }
-        if (lastKeyPressed === 'equals')
-        {
+        if (lastKeyPressed === 'equals') {
             numbersInCalculation.pop();
         }
         populateDisplay();
         numbersInCalculation.pop();
         console.log(numbersInCalculation);
-    }
-    else
-    {
+    } else {
         displayValue = '';
     }
     switch (e.target.id) {
         case 'plus':
-        operator = 'add'
-        break;
+            operator = 'add';
+            break;
 
         case 'addition':
-        operator = 'add'
-        break;
+            operator = 'add';
+            break;
 
         case 'minus':
-        operator = 'subtract'
-        break;
+            operator = 'subtract';
+            break;
 
         case 'subtraction':
-        operator = 'subtract'
-        break;
+            operator = 'subtract';
+            break;
 
         case 'times':
-        operator = 'multiply'
-        break;
+            operator = 'multiply';
+            break;
 
         case 'multiplication':
-        operator = 'multiply'
-        break;
+            operator = 'multiply';
+            break;
 
         case 'division':
-        operator = 'divide'
-        break;
+            operator = 'divide';
+            break;
 
         case 'dividedby':
-        operator = 'divide'
-        break;
+            operator = 'divide';
+            break;
     }
-    lastKeyPressed = 'operator'
-    console.log(operator)
-        return operator;
+    lastKeyPressed = 'operator';
+    console.log(operator);
+    return operator;
 }
 
 function addHighlight(e) {
-    if (e.target.classList.contains("column"))
-    {
+    if (e.target.classList.contains('column')) {
         e.target.classList.add('highlight');
-    }
-    else
-    {
+    } else {
         e.target.parentNode.classList.add('highlight');
     }
 }
 
 function removeHighlight(e) {
-    if (e.target.classList.contains("column"))
-    {
+    if (e.target.classList.contains('column')) {
         e.target.classList.remove('highlight');
-    }
-    else
-    {
+    } else {
         e.target.parentNode.classList.remove('highlight');
     }
 }
 
 function enactModulator(e) {
-    if (lastKeyPressed != 'number')
-    {
+    if (lastKeyPressed != 'number') {
         return;
     }
-    if (e.target.textContent.includes('%'))
-    {
+    if (e.target.textContent.includes('%')) {
         displayValue = displayValue * 0.01;
         populateDisplay();
-    }
-    else
-    {
+    } else {
         displayValue = displayValue * -1;
         populateDisplay();
     }
